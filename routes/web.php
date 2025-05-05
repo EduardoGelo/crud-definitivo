812e6390dc74060ef87793589d8ed0d4a2a46418
EduardoGelo <eduardo24games@gmail.com>
2025-05-05 20:20:57 -0300

    Rotas mais organizadas, precisa colocar as middleware. acrescentado um is_admin na migration de users

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // colocar tudo aqui
 });
-
-Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+//crud de clientes
+Route::middleware('auth')->group(function () {
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
     Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
     Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
     Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show');
     Route::get('/clientes/{cliente}/edit', [ClienteController::class, 'edit'])->name('clientes.edit');
     Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
     Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
-
+});
+//crud de produtos
+Route::middleware('auth')->group(function () {
     Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
     Route::get('/produtos/create', [ProdutoController::class, 'create'])->name('produtos.create');
     Route::post('/produtos', [ProdutoController::class, 'store'])->name('produtos.store');
@@ -35,7 +39,8 @@ Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.ind
     Route::get('/produtos/{produto}/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
     Route::put('/produtos/{produto}', [ProdutoController::class, 'update'])->name('produtos.update');
     Route::delete('/produtos/{produto}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
-
+});
+//crud de categorias
 Route::middleware('auth')->group(function () {
     Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
     Route::get('/categorias/create', [CategoriaController::class, 'create'])->name('categorias.create');
